38a7d6d16d44018c82f7ee16457ec48b2d4ebba3
yoshikikasama <kasamayoshiki@gmail.com>
2024-11-02 10:56:35 +0900

    devcontainer

---
+++ SQL/thorough_sql_instructions_from_the_masters/CREATE TABLE users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY
+    ,username VARCHAR(50) NOT NULL
+    ,email VARCHAR(100) NOT NULL
+    ,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- サンプルデータの挿入
+INSERT INTO users (username, email) VALUES 
+('alice', 'alice@example.com'),
+('bob', 'bob@example.com'),
+('charlie', 'charlie@example.com');
